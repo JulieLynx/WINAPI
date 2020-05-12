cdd2fe7181736a8e44ab6df0cb4ff34ee99920c4
Julie <Julie@JULIE->
2020-05-12 19:56:55 +0300

    Add project files.

---
+++ SimpleWindow/main.cpp
@@ -0,0 +1,129 @@
+#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR SZ_CLASS_NAME[] = "MyWindowClass";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	//1) Регистрация класса окна:
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wc.lpszClassName = SZ_CLASS_NAME;
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	//2) Создание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		WS_EX_CLIENTEDGE,
+		SZ_CLASS_NAME,
+		"This is my first Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window not created", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	//3) Цикл сообщений (Message loop)
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	
+	case WM_CREATE:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL),
+			MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+	}
+	break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_EXIT:
+		{
+			DestroyWindow(hwnd);
+		}
+		case ID_HELP_ABOUT:
+		{
+			//MessageBox(hwnd, "about", "Info", MB_OK);
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+			//CreateDialog()
+		}
+		}
+	}
+	break;
+
+	case WM_CLOSE:
+		if (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Че, реально?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		{
+			DestroyWindow(hwnd);
+		}
+		break;
+	case WM_DESTROY:
+		MessageBox(hwnd, "Лучше б двери закрыли", "Info", MB_OK);
+		PostQuitMessage(0);
+		break;
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+	}
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
